839a9d85cebf0cdf03b39ce139284a83999043d3
twof <fabiobean2@gmail.com>
2018-03-12 23:11:34 -0700

    Add struct mailgun engine

--- Sources/Mailgun/Mailgun.swift
+++ Sources/Mailgun/Mailgun.swift
@@ -17,10 +17,11 @@ public struct MailgunFormData: Content {
     }
 }
 
-public protocol Mailgun: class, Service {
+public protocol Mailgun: Service {
     var apiKey: String { get }
     var customURL: String { get }
-    func sendMail(data content: MailgunFormData, on req: Request) throws -> Future<Response>
+    var numMailSent: Int { get set }
+    mutating func sendMail(data content: MailgunFormData, on req: Request) throws -> Future<Response>
 }
 
 public class MailgunEngine: Mailgun {
@@ -50,6 +51,40 @@ public class MailgunEngine: Mailgun {
             .post(mailgunURL, headers: headers, content: content)
             .map(to: Response.self) { (response) in
                 self.numMailSent += 1
+                return response
+            }.catch { (err) in
+                print(err)
+        }
+    }
+}
+
+public struct MailgunStructEngine: Mailgun {
+    public var apiKey: String
+    public var customURL: String
+    public var numMailSent: Int = 0
+    
+    public init(apiKey: String, customURL: String) {
+        self.apiKey = apiKey
+        self.customURL = customURL
+    }
+    
+    public mutating func sendMail(data content: MailgunFormData, on req: Request) throws -> Future<Response> {
+        let client = try req.make(EngineClient.self)
+        let encode: (String) -> String = Base64Encoder(encoding: .base64).encode
+        let authKey = encode("api:key-\(self.apiKey)")
+        
+        let headers = HTTPHeaders(dictionaryLiteral:
+            (HTTPHeaderName.authorization, "Basic \(authKey)"),
+                                  (HTTPHeaderName.contentType, "application/x-www-form-urlencoded")
+        )
+        
+        let mailgunURL = "https://api.mailgun.net/v3/\(self.customURL)/messages"
+        
+        self.numMailSent += 1
+        
+        return client
+            .post(mailgunURL, headers: headers, content: content)
+            .map(to: Response.self) { (response) in
                 return response
             }.catch { (err) in
                 print(err)
