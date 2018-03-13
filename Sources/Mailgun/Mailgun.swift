264576bf1ed3a5f44d44713fbc2c3603177ebc6a
twof <fabiobean2@gmail.com>
2018-03-13 07:45:38 -0700

    Back into a useable state

--- Sources/Mailgun/Mailgun.swift
+++ Sources/Mailgun/Mailgun.swift
@@ -17,17 +17,15 @@ public struct MailgunFormData: Content {
     }
 }
 
-public protocol Mailgun: Service {
+public protocol MailgunProvider: Service {
     var apiKey: String { get }
     var customURL: String { get }
-    var numMailSent: Int { get set }
-    mutating func sendMail(data content: MailgunFormData, on req: Request) throws -> Future<Response>
+    func sendMail(data content: MailgunFormData, on req: Request) throws -> Future<Response>
 }
 
-public class MailgunEngine: Mailgun {
+public struct MailgunEngine: MailgunProvider {
     public var apiKey: String
     public var customURL: String
-    public var numMailSent: Int = 0
     
     public init(apiKey: String, customURL: String) {
         self.apiKey = apiKey
@@ -45,42 +43,6 @@ public class MailgunEngine: Mailgun {
         )
         
         let mailgunURL = "https://api.mailgun.net/v3/\(self.customURL)/messages"
-        
-        
-        return client
-            .post(mailgunURL, headers: headers, content: content)
-            .map(to: Response.self) { (response) in
-                self.numMailSent += 1
-                return response
-            }.catch { (err) in
-                print(err)
-        }
-    }
-}
-
-public struct MailgunStructEngine: Mailgun {
-    public var apiKey: String
-    public var customURL: String
-    public var numMailSent: Int = 0
-    
-    public init(apiKey: String, customURL: String) {
-        self.apiKey = apiKey
-        self.customURL = customURL
-    }
-    
-    public mutating func sendMail(data content: MailgunFormData, on req: Request) throws -> Future<Response> {
-        let client = try req.make(EngineClient.self)
-        let encode: (String) -> String = Base64Encoder(encoding: .base64).encode
-        let authKey = encode("api:key-\(self.apiKey)")
-        
-        let headers = HTTPHeaders(dictionaryLiteral:
-            (HTTPHeaderName.authorization, "Basic \(authKey)"),
-                                  (HTTPHeaderName.contentType, "application/x-www-form-urlencoded")
-        )
-        
-        let mailgunURL = "https://api.mailgun.net/v3/\(self.customURL)/messages"
-        
-        self.numMailSent += 1
         
         return client
             .post(mailgunURL, headers: headers, content: content)
