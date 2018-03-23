ad27802298ddb5959cda8ac1c5e080954f385fcc
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-03-23 13:48:37 +0000

    making it better

--- Sources/Mailgun/Mailgun.swift
+++ Sources/Mailgun/Mailgun.swift
@@ -1,62 +1,117 @@
 import Vapor
 import Foundation
 
+
+// MARK: - Service
+
 public protocol MailgunProvider: Service {
     var apiKey: String { get }
-    var customURL: String { get }
-    func send(data content: MailgunEngine.FormData, on req: Request) throws -> Future<Response>
+    var domain: String { get }
+    func send(_ content: Mailgun.Message, on req: Request) throws -> Future<Response>
 }
 
-public struct MailgunEngine: MailgunProvider {
+// MARK: - Engine
+
+public struct Mailgun: MailgunProvider {
     
-    public enum MailgunError: Error {
+    public enum Problem: Error {
         case encodingProblem
     }
     
-    public struct FormData: Content {
-        public static let defaultMediaType: MediaType = MediaType.urlEncodedForm
+    public struct Message: Content {
         
-        let from: String
-        let to: String
-        let subject: String
-        let text: String
+        public typealias FullEmail = (email: String, name: String?)
         
-        public init(from: String, to: String, subject: String, text: String) {
+        public let from: String
+        public let to: String
+        public let cc: String?
+        public let bcc: String?
+        public let subject: String
+        public let text: String
+        public let html: String?
+        public let attachment: [Data]
+        
+        public init(from: String, to: String, cc: String? = nil, bcc: String? = nil, subject: String, text: String, html: String? = nil, attachment: [Data] = []) {
             self.from = from
             self.to = to
+            self.cc = cc
+            self.bcc = bcc
             self.subject = subject
             self.text = text
+            self.html = html
+            self.attachment = attachment
         }
+        
+        public init(from: String, to: [String], cc: [String]? = nil, bcc: [String]? = nil, subject: String, text: String, html: String? = nil, attachment: [Data] = []) {
+            self.from = from
+            self.to = to.joined(separator: ",")
+            self.cc = cc?.joined(separator: ",")
+            self.bcc = bcc?.joined(separator: ",")
+            self.subject = subject
+            self.text = text
+            self.html = html
+            self.attachment = attachment
+        }
+        
+        public init(from: String, to: [FullEmail], cc: [FullEmail]? = nil, bcc: [FullEmail]? = nil, subject: String, text: String, html: String? = nil, attachment: [Data] = []) {
+            self.from = from
+            self.to = to.stringArray.joined(separator: ",")
+            self.cc = cc?.stringArray.joined(separator: ",")
+            self.bcc = bcc?.stringArray.joined(separator: ",")
+            self.subject = subject
+            self.text = text
+            self.html = html
+            self.attachment = attachment
+        }
+        
     }
     
-    public var apiKey: String
-    public var customURL: String
+    public let apiKey: String
+    public let domain: String
     
-    public init(apiKey: String, customURL: String) {
+    // MARK: Initialization
+    
+    public init(apiKey: String, domain: String) {
         self.apiKey = apiKey
-        self.customURL = customURL
+        self.domain = domain
     }
     
-    public func send(data content: FormData, on req: Request) throws -> Future<Response> {
-        guard let apiKeyData = "api:key-\(self.apiKey)".data(using: .utf8) else {
-            throw MailgunError.encodingProblem
+    // MARK: Send message
+    
+    public func send(_ content: Message, on req: Request) throws -> Future<Response> {
+        let key = apiKey.contains("key-") ? apiKey : "key-\(apiKey)"
+        guard let apiKeyData = "api:key-\(key)".data(using: .utf8) else {
+            throw Problem.encodingProblem
         }
         let authKey = apiKeyData.base64EncodedData()
         guard let authKeyEncoded = String.init(data: authKey, encoding: .utf8) else {
-            throw MailgunError.encodingProblem
+            throw Problem.encodingProblem
         }
         
         var headers = HTTPHeaders([])
         headers.add(name: HTTPHeaderName.authorization, value: "Basic \(authKeyEncoded)")
-        headers.add(name: HTTPHeaderName.contentType, value: "application/x-www-form-urlencoded")
+        let contentType = content.attachment.isEmpty ? MediaType.urlEncodedForm.description : MediaType.multipart.description
+        headers.add(name: HTTPHeaderName.contentType, value: contentType)
         
-        let mailgunURL = "https://api.mailgun.net/v3/\(self.customURL)/messages"
+        let mailgunURL = "https://api.mailgun.net/v3/\(domain)/messages"
         
         let client = try req.make(Client.self)
-        return client
-            .post(mailgunURL, headers: headers, content: content)
-            .map(to: Response.self) { (response) in
-                return response
-        }
+        return client.post(mailgunURL, headers: headers, content: content)
     }
+    
+}
+
+// MARK: - Conversions
+
+extension Array where Element == Mailgun.Message.FullEmail {
+    
+    var stringArray: [String] {
+        return map({ entry -> String in
+            guard let name = entry.name else {
+                return entry.email
+            }
+            return "\"\(name) <\(entry.email)>\""
+        })
+    }
+    
 }
