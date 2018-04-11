89e51fb60c9417cee0a639a564e78788671c25e0
twof <fabiobean2@gmail.com>
2018-04-10 21:11:20 -0700

    Move Model to its own file

--- Sources/Mailgun/Mailgun.swift
+++ Sources/Mailgun/Mailgun.swift
@@ -20,51 +20,6 @@ public struct Mailgun: MailgunProvider {
         case unableToSendEmail
     }
     
-    public struct Message: Content {
-        public static var defaultMediaType: MediaType = MediaType.urlEncodedForm
-        
-        public typealias FullEmail = (email: String, name: String?)
-        
-        public let from: String
-        public let to: String
-        public let cc: String?
-        public let bcc: String?
-        public let subject: String
-        public let text: String
-        public let html: String?
-        
-        public init(from: String, to: String, cc: String? = nil, bcc: String? = nil, subject: String, text: String, html: String? = nil) {
-            self.from = from
-            self.to = to
-            self.cc = cc
-            self.bcc = bcc
-            self.subject = subject
-            self.text = text
-            self.html = html
-        }
-        
-        public init(from: String, to: [String], cc: [String]? = nil, bcc: [String]? = nil, subject: String, text: String, html: String? = nil) {
-            self.from = from
-            self.to = to.joined(separator: ",")
-            self.cc = cc?.joined(separator: ",")
-            self.bcc = bcc?.joined(separator: ",")
-            self.subject = subject
-            self.text = text
-            self.html = html
-        }
-        
-        public init(from: String, to: [FullEmail], cc: [FullEmail]? = nil, bcc: [FullEmail]? = nil, subject: String, text: String, html: String? = nil) {
-            self.from = from
-            self.to = to.stringArray.joined(separator: ",")
-            self.cc = cc?.stringArray.joined(separator: ",")
-            self.bcc = bcc?.stringArray.joined(separator: ",")
-            self.subject = subject
-            self.text = text
-            self.html = html
-        }
-        
-    }
-    
     public let apiKey: String
     public let domain: String
     
@@ -123,5 +78,4 @@ extension Array where Element == Mailgun.Message.FullEmail {
             return "\"\(name) <\(entry.email)>\""
         })
     }
-    
 }
---
+++ Sources/Mailgun/Models/Message.swift
@@ -0,0 +1,48 @@
+import Vapor
+
+extension Mailgun {
+    public struct Message: Content {
+        public static var defaultMediaType: MediaType = MediaType.urlEncodedForm
+        
+        public typealias FullEmail = (email: String, name: String?)
+        
+        public let from: String
+        public let to: String
+        public let cc: String?
+        public let bcc: String?
+        public let subject: String
+        public let text: String
+        public let html: String?
+        
+        public init(from: String, to: String, cc: String? = nil, bcc: String? = nil, subject: String, text: String, html: String? = nil) {
+            self.from = from
+            self.to = to
+            self.cc = cc
+            self.bcc = bcc
+            self.subject = subject
+            self.text = text
+            self.html = html
+        }
+        
+        public init(from: String, to: [String], cc: [String]? = nil, bcc: [String]? = nil, subject: String, text: String, html: String? = nil) {
+            self.from = from
+            self.to = to.joined(separator: ",")
+            self.cc = cc?.joined(separator: ",")
+            self.bcc = bcc?.joined(separator: ",")
+            self.subject = subject
+            self.text = text
+            self.html = html
+        }
+        
+        public init(from: String, to: [FullEmail], cc: [FullEmail]? = nil, bcc: [FullEmail]? = nil, subject: String, text: String, html: String? = nil) {
+            self.from = from
+            self.to = to.stringArray.joined(separator: ",")
+            self.cc = cc?.stringArray.joined(separator: ",")
+            self.bcc = bcc?.stringArray.joined(separator: ",")
+            self.subject = subject
+            self.text = text
+            self.html = html
+        }
+    }
+}
+
