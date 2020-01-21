320b34bb05f97eb7547266d3e8092837de358a92
Mihael Isaev <isaev.mihael@gmail.com>
2020-01-22 02:28:35 +0400

    IncomingMessage: implement attachments decoding

--- Sources/Mailgun/Models/IncomingMessage.swift
+++ Sources/Mailgun/Models/IncomingMessage.swift
@@ -14,7 +14,7 @@ public struct MailgunIncomingMessage: Content {
     public let strippedHTML: String
     public let messageHeaders: String
     public let contentIdMap: String
-    public let attachments: [Attachment]?
+    public let attachments: [File]
     
     enum CodingKeys: String, CodingKey {
         case recipient
@@ -29,6 +29,46 @@ public struct MailgunIncomingMessage: Content {
         case messageHeaders = "message-headers"
         case contentIdMap = "content-id-map"
         case attachments
+    }
+    
+    struct DynamicAttachmentKey: CodingKey {
+        var stringValue: String
+        
+        init?(stringValue: String) {
+            guard stringValue.hasPrefix("attachment-") else { return nil }
+            guard let lastKey = stringValue.components(separatedBy: "-").last,
+                let _ = Int(lastKey)
+                else { return nil}
+            self.stringValue = stringValue
+        }
+        
+        var intValue: Int?
+        
+        init?(intValue: Int) {
+            return nil
+        }
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        recipient = try container.decode(String.self, forKey: .recipient)
+        sender = try container.decode(String.self, forKey: .sender)
+        from = try container.decode(String.self, forKey: .from)
+        subject = try container.decode(String.self, forKey: .subject)
+        bodyPlain = try container.decode(String.self, forKey: .bodyPlain)
+        strippedText = try container.decode(String.self, forKey: .strippedText)
+        strippedSignature = try container.decodeIfPresent(String.self, forKey: .strippedSignature)
+        bodyHTML = try container.decode(String.self, forKey: .bodyHTML)
+        strippedHTML = try container.decode(String.self, forKey: .strippedHTML)
+        messageHeaders = try container.decode(String.self, forKey: .messageHeaders)
+        contentIdMap = try container.decode(String.self, forKey: .contentIdMap)
+
+        var _attachments: [File] = []
+        let attachmentsContainer = try decoder.container(keyedBy: DynamicAttachmentKey.self)
+        try attachmentsContainer.allKeys.forEach { attachmentKey in
+            _attachments.append(try attachmentsContainer.decode(File.self, forKey: attachmentKey))
+        }
+        attachments = _attachments
     }
 }
 
