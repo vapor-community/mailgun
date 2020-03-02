2ca4ce909467c7a04a690db0f1786bf36dc893e1
Mihael Isaev <isaev.mihael@gmail.com>
2020-03-02 17:57:17 +0400

    Switch to Vapor4 RC

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Mailgun",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_15)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["Mailgun"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.3")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
