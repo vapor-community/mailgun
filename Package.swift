b608edc1200cd974bd4df9edf458fe497e01ce06
Mads Odgaard <mads@madsodgaard.com>
2020-04-09 20:29:08 +0200

    Update to vapor 4.0.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["Mailgun"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .branch("gm"))
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
